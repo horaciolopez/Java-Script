9d1b964d5455c88e25052bdad91cd6d186924de4
Horacio Lopez <reparatupcvm@gmail.com>
2024-03-12 23:42:19 -0300

    sentencias

---
+++ 02-operadores/07-orden.js
@@ -0,0 +1,8 @@
+//let resultado = 8/2 (2+2);
+
+//console.log(resultado);
+// en console mostrara error.
+
+let resultado = 8/2 * (2+2);
+
+console.log(resultado);
---
+++ 03-control-flujo/02-else.js
@@ -0,0 +1,11 @@
+let edad = 15;
+
+if(edad > 17){
+    console.log("Usuario mayor de edad");
+}else if (edad > 13){
+    console.log("Usuario necesita ser acompañado de sus padres");
+
+}else {
+    console.log("usuario menor de edad");
+}
+
