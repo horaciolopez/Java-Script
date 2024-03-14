08909c3310cf00e45846d87f6139304918897f5d
Horacio Lopez <reparatupcvm@gmail.com>
2024-03-14 11:02:59 -0300

    ej 2

--- 04-ejercicios/02.js
+++ 04-ejercicios/02.js
@@ -9,6 +9,8 @@
 function nombreResolucion(ancho, alto){
     if (ancho >= 7680 && alto >= 4320){
        return "8K"; 
+    }else if (ancho >= 3840 && alto >= 2160){
+        return "4K",
     }
     
 }
